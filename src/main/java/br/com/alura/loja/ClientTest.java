7ccd6231feb8d22bae573304827504da382c70a2
Wesley Machado <weslley182@gmail.com>
2016-04-10 23:11:21 -0300

    COmmit inicial

---
+++ src/main/java/br/com/alura/loja/ClientTest.java
@@ -0,0 +1,67 @@
+package br.com.alura.loja;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.thoughtworks.xstream.XStream;
+
+import br.com.alura.loja.modelo.Carrinho;
+import br.com.alura.loja.modelo.Projeto;
+
+public class ClientTest {
+	private HttpServer server;
+	
+	@Before
+	public void iniciaServidor(){
+		this.server = Servidor.inicializaServidor();
+	}
+	
+	@After
+	public void finalizaServidor(){
+		server.stop();
+	}
+	
+	@Test
+	public void testaQueAConexaoComOServidorFunciona(){
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target("http://www.mocky.io");
+		
+		String conteudo = target.path("/v2/52aaf5deee7ba8c70329fb7d").request().get(String.class);
+		
+		Assert.assertTrue(conteudo.contains("<rua>Rua Vergueiro 3185"));		
+	}
+	
+	@Test
+	public void testaQueAConexaoComOServidorFuncionaNoPathDeProjetos() {		
+        Client client = ClientBuilder.newClient();        
+        WebTarget target = client.target("http://localhost:8080");        
+        String conteudo = target.path("/projetos").request().get(String.class);        
+        Assert.assertTrue(conteudo.contains("<nome>Minha loja"));
+    }
+	
+	@Test
+	public void testaQueBuscarUmCarrinhoTrazOCarrinhoEsperado() {		
+        Client client = ClientBuilder.newClient();        
+        WebTarget target = client.target("http://localhost:8080");        
+        String conteudo = target.path("/carrinhos").request().get(String.class);
+        Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
+        System.out.println(conteudo);
+        Assert.assertTrue(conteudo.contains("<rua>Rua Vergueiro 3185"));
+    }
+	
+	@Test
+    public void testaQueBuscarUmProjetoTrazOProjetoEsperado() {
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target("http://localhost:8080");
+        String conteudo = target.path("/projetos").request().get(String.class);
+        Projeto projeto = (Projeto) new XStream().fromXML(conteudo);
+        Assert.assertEquals("Minha loja", projeto.getNome());
+    }
+}
---
+++ src/main/java/br/com/alura/loja/modelo/Projeto.java
@@ -0,0 +1,34 @@
+package br.com.alura.loja.modelo;
+
+import com.thoughtworks.xstream.XStream;
+
+public class Projeto {
+	
+	private String nome;
+	public String getNome() {
+		return nome;
+	}
+
+	private long id;
+	private long anoDeInicio;
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Projeto(long id, String nome, long anoDeInicio) {
+		this.nome = nome;
+		this.id = id;
+		this.anoDeInicio = anoDeInicio;
+	}
+	
+	public Projeto() {
+	}
+	
+	public String toXML(){
+		return new XStream().toXML(this);
+	}
+	
+	
+
+}
